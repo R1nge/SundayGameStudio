e8d464a3c5c191be6ebcbd9aaedcd202f51737ae
R1nge <r1ngegamedev@gmail.com>
2023-06-20 07:02:35 +0300

    Task 2.3 added footprints

---
+++ Assets/Task_2_3/Scripts/Footprint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Task_2_3.Scripts
+{
+    public class Footprint : MonoBehaviour
+    {
+        [SerializeField] private new ParticleSystem particleSystem;
+
+        private void OnEnable()
+        {
+            particleSystem.Play();
+            Invoke(nameof(Hide), particleSystem.main.duration);
+        }
+
+        private void Hide() => gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Task_2_3/Scripts/FootprintController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Task_2_3.Scripts
+{
+    public class FootprintController : MonoBehaviour
+    {
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private Transform playerModel;
+        [SerializeField] private ParticleSystem system;
+        [SerializeField] private float delta = 1;
+        [SerializeField] private float gap = 0.5f;
+        private int _dir = 1;
+        private Vector3 _lastEmit;
+
+        private void Start() => _lastEmit = playerModel.transform.position;
+
+        private void Update()
+        {
+            if (Vector3.Distance(_lastEmit, playerModel.transform.position) > delta)
+            {
+                var pos = playerModel.transform.position + playerModel.transform.right * (gap * _dir) + offset;
+                _dir *= -1;
+                ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams
+                {
+                    position = pos,
+                    rotation = playerModel.transform.rotation.eulerAngles.y
+                };
+                system.Emit(emitParams, 1);
+                _lastEmit = playerModel.transform.position;
+            }
+        }
+    }
+}
